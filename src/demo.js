cc6407b2ab94feeee61b85c4ad8c99d2b18d669d
Shohsuvor Abdusamiyev <shohsuvor@MacBook-Pro.local>
2023-07-13 18:23:20 +0500

    add some material

--- src/demo.js
+++ src/demo.js
@@ -29,20 +29,38 @@ function subtotal(items) {
 
 export default function SpanningTable() {
   const [first, setfirst] = React.useState({
+    MM6: 0,
     MM9: 0,
     MM12: 0,
     MM16: 0,
     MM20: 0,
     MM20: 0,
     MM22: 0,
+    MM25: 0,
+    MM28: 0,
+    MM32: 0,
+    MM40: 0,
+    MM50: 0,
+    MM63: 0,
+    MM75: 0,
+    MM90: 0,
   });
 
   const rows = [
-    createRow("теплоизоляционные трубки 9мм", 0.17, first.MM9),
-    createRow("теплоизоляционные трубки 12мм", 0.21, first.MM12),
-    createRow("теплоизоляционные трубки 16мм", 0.25, first.MM16),
-    createRow("теплоизоляционные трубки 20мм", 0.29, first.MM20),
-    createRow("теплоизоляционные трубки 22мм", 0.33, first.MM22),
+    createRow("теплоизоляционные трубки 6мм", 0.17, first.MM6),
+    createRow("теплоизоляционные трубки 9мм", 0.21, first.MM9),
+    createRow("теплоизоляционные трубки 12мм", 0.25, first.MM12),
+    createRow("теплоизоляционные трубки 16мм", 0.29, first.MM16),
+    createRow("теплоизоляционные трубки 20мм", 0.33, first.MM20),
+    createRow("теплоизоляционные трубки 22мм", 0.34, first.MM22),
+    createRow("теплоизоляционные трубки 25мм", 0.40, first.MM25),
+    createRow("теплоизоляционные трубки 28мм", 0.41, first.MM28),
+    createRow("теплоизоляционные трубки 32мм", 0.43, first.MM32),
+    createRow("теплоизоляционные трубки 40мм", 0.57, first.MM40),
+    createRow("теплоизоляционные трубки 50мм", 0.83, first.MM50),
+    createRow("теплоизоляционные трубки 63мм", 1.12, first.MM63),
+    createRow("теплоизоляционные трубки 75мм", 1.45, first.MM75),
+    createRow("теплоизоляционные трубки 90мм", 1.75, first.MM90),
   ];
   const invoiceSubtotal = subtotal(rows);
 
@@ -65,7 +83,7 @@ export default function SpanningTable() {
             {rows.map((row) => (
               <TableRow key={row.desc}>
                 <TableCell>{row.desc}</TableCell>
-                <TableCell align="right">{row.qty} $</TableCell>
+                <TableCell align="right">{ccyFormat(row.qty)} $</TableCell>
                 <div className="input">
                   <input
                     onChange={(e) => {
@@ -114,6 +132,24 @@ export default function SpanningTable() {
               +ccyFormat(rows[3].price),
               +ccyFormat(rows[4].unit),
               +ccyFormat(rows[4].price),
+              +ccyFormat(rows[5].unit),
+              +ccyFormat(rows[5].price),
+              +ccyFormat(rows[6].unit),
+              +ccyFormat(rows[6].price),
+              +ccyFormat(rows[7].unit),
+              +ccyFormat(rows[7].price),
+              +ccyFormat(rows[8].unit),
+              +ccyFormat(rows[8].price),
+              +ccyFormat(rows[9].unit),
+              +ccyFormat(rows[9].price),
+              +ccyFormat(rows[10].unit),
+              +ccyFormat(rows[10].price),
+              +ccyFormat(rows[11].unit),
+              +ccyFormat(rows[11].price),
+              +ccyFormat(rows[12].unit),
+              +ccyFormat(rows[12].price),
+              +ccyFormat(rows[13].unit),
+              +ccyFormat(rows[13].price),
               +ccyFormat(invoiceSubtotal)
             );
           }}
