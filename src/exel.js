27f877a902fb182c7fc5a8f18058c461356b1b34
shaxme <abdusamiyev10@gmail.com>
2023-07-15 13:15:55 +0500

    edit some exel codes

--- src/exel.js
+++ src/exel.js
@@ -1,61 +1,63 @@
 const XLSX = require("xlsx");
 const date = new Date();
+
 function createDox(
-    MM6,
-    MM6Total,
-    MM9,
-    MM9Total,
-    MM12,
-    MM12Total,
-    MM16,
-    MM16Total,
-    MM20,
-    MM20Total,
-    MM22,
-    MM22Total,
-    MM25,
-    MM25Total,
-    MM28,
-    MM28Total,
-    MM32,
-    MM32Total,
-    MM40,
-    MM40Total,
-    MM50,
-    MM50Total,
-    MM63,
-    MM63Total,
-    MM75,
-    MM75Total,
-    MM90,
-    MM90Total,
-    Total
-
+  MM6,
+  MM6Total,
+  MM9,
+  MM9Total,
+  MM12,
+  MM12Total,
+  MM16,
+  MM16Total,
+  MM20,
+  MM20Total,
+  MM22,
+  MM22Total,
+  MM25,
+  MM25Total,
+  MM28,
+  MM28Total,
+  MM32,
+  MM32Total,
+  MM40,
+  MM40Total,
+  MM50,
+  MM50Total,
+  MM63,
+  MM63Total,
+  MM75,
+  MM75Total,
+  MM90,
+  MM90Total,
+  Total
 ) {
-    const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.aoa_to_sheet([
-        ["НАКЛАДНОЙ ЛИСТ", "цена", "Кол-во", "Сумма"],
-        ["Теплоизоляционные трубки 6 мм", "0.17 $", MM6, MM6Total + " $"],
-        ["Теплоизоляционные трубки 25 мм", "0.21 $", MM9, MM9Total + " $"],
-        ["Теплоизоляционные трубки 12 мм", "0.25 $", MM12, MM12Total + " $"],
-        ["Теплоизоляционные трубки 16 мм", "0.29 $", MM16, MM16Total + " $"],
-        ["Теплоизоляционные трубки 20 мм", "0.33 $", MM20, MM20Total + " $"],
-        ["Теплоизоляционные трубки 22 мм", "0.34 $", MM22, MM22Total + " $"],
-        ["теплоизоляционные трубки 25 мм", "0.40 $", MM25, MM25Total + " $"],
-        ["теплоизоляционные трубки 28 мм", "0.41 $", MM28, MM28Total + " $"],
-        ["теплоизоляционные трубки 32 мм", "0.43 $", MM32, MM32Total + " $"],
-        ["теплоизоляционные трубки 40 мм", "0.57 $", MM40, MM40Total + " $"],
-        ["теплоизоляционные трубки 50 мм", "0.83 $", MM50, MM50Total + " $"],
-        ["теплоизоляционные трубки 63 мм", "1.12 $", MM63, MM63Total + " $"],
-        ["теплоизоляционные трубки 75 мм", "1.45 $", MM75, MM75Total + " $"],
-        ["теплоизоляционные трубки 90 мм", "1.75 $", MM90, MM90Total + " $"],
-        ["ИТОГ:", "", "", Total + " $"],
-        ['', '', '', String(date.getDate()).padStart(2, 0) + "." + String(date.getMonth()).padStart(2, 0) + "." + String(date.getFullYear()).padStart(2, 0)],
-    ]);
-    const columnWidths = [{ wch: 35 }];
-    worksheet["!cols"] = columnWidths;
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.aoa_to_sheet([
+    ["НАКЛАДНОЙ ЛИСТ", "цена", "Кол-во", "Сумма"],
+    ...(MM6 !== 0 ? [["Теплоизоляционные трубки 6 мм", "0.17 $", MM6, MM6Total + " $"]] : []),
+    ...(MM9 !== 0 ? [["Теплоизоляционные трубки 25 мм", "0.21 $", MM9, MM9Total + " $"]] : []),
+    ...(MM12 !== 0 ? [["Теплоизоляционные трубки 12 мм", "0.25 $", MM12, MM12Total + " $"]] : []),
+    ...(MM16 !== 0 ? [["Теплоизоляционные трубки 16 мм", "0.29 $", MM16, MM16Total + " $"]] : []),
+    ...(MM20 !== 0 ? [["Теплоизоляционные трубки 20 мм", "0.33 $", MM20, MM20Total + " $"]] : []),
+    ...(MM22 !== 0 ? [["Теплоизоляционные трубки 22 мм", "0.34 $", MM22, MM22Total + " $"]] : []),
+    ...(MM25 !== 0 ? [["теплоизоляционные трубки 25 мм", "0.40 $", MM25, MM25Total + " $"]] : []),
+    ...(MM28 !== 0 ? [["теплоизоляционные трубки 28 мм", "0.41 $", MM28, MM28Total + " $"]] : []),
+    ...(MM32 !== 0 ? [["теплоизоляционные трубки 32 мм", "0.43 $", MM32, MM32Total + " $"]] : []),
+    ...(MM40 !== 0 ? [["теплоизоляционные трубки 40 мм", "0.57 $", MM40, MM40Total + " $"]] : []),
+    ...(MM50 !== 0 ? [["теплоизоляционные трубки 50 мм", "0.83 $", MM50, MM50Total + " $"]] : []),
+    ...(MM63 !== 0 ? [["теплоизоляционные трубки 63 мм", "1.12 $", MM63, MM63Total + " $"]] : []),
+    ...(MM75 !== 0 ? [["теплоизоляционные трубки 75 мм", "1.45 $", MM75, MM75Total + " $"]] : []),
+    ...(MM90 !== 0 ? [["теплоизоляционные трубки 90 мм", "1.75 $", MM90, MM90Total + " $"]] : []),
+    ["ИТОГ:", "", "", Total + " $"],
+    ["", "", "", String(date.getDate()).padStart(2, 0) + "." + String(date.getMonth()).padStart(2, 0) + "." + String(date.getFullYear()).padStart(2, 0)],
+  ]);
 
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-    XLSX.writeFile(workbook, "output.xlsx");
+  const columnWidths = [{ wch: 35 }];
+  worksheet["!cols"] = columnWidths;
+
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+  XLSX.writeFile(workbook, "output.xlsx");
 }
+
 module.exports = createDox;
